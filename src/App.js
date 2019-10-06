a6064576f75ec5007c5c7a42d159156f59dd86ea
Francis Bulus <notchera@gmail.com>
2019-10-06 22:47:04 +0100

    created cart context and refactorednavigation component code to use context api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
-import {ProductContext} from "./Contexts/ProductContext";
+import { ProductContext } from "./Contexts/ProductContext";
+import { CartContext } from "./Contexts/CartContext";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -19,16 +20,14 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <CartContext.Provider value={cart}>
+          <Navigation cart={cart} />
 
-        {/* Routes */}
-        <Route
-          exact
-          path="/"
-         component={Products}
-        />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
 
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
